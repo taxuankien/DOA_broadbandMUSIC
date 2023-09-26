546341d63cde1d5f38fdd6a85ff7afdb9bf59966
cien <taxuankien2k2@gmail.com>
2023-09-26 21:35:51 +0700

    commit main.py

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from scipy.fft import fft, ifft
 import time
 import socket
 from broadbandMUSIC import *
-import wave
 
 elements_distance = 0.064
-sound_velo =    348
+sound_velo =    346
 num_sources =   1
 num_elements =  4
 
-samp_rate =     16000
-FRAME_SIZE =    800
+samp_rate =     48000
+FRAME_SIZE =    2400
 SAMP_WIDTH =    2   #bytes
 CHUNK =         4*FRAME_SIZE #Bytes 4 channel 2 bytes
 
@@ -29,22 +28,19 @@ print("Connecting to port")
 try:
     server_socket.connect(server_address)
     print("Connected! Ready to receive data.")
-    t = time.time()
-    
-    dec_data = np.frombuffer(recv_data(CHUNK* SAMP_WIDTH , server_socket), dtype= np.int16)
-
-    # ham tinh tri so quyet dinh
+    # t = time.time()
+    # nguong cong suat quyet dinh nguon am duoc xu ly
     threshold = 15
     
     freqs = np.fft.fftfreq(FRAME_SIZE * 2, d= 1/ samp_rate)
     angles = np.linspace(-np.pi/2, np.pi/2, 181)
     array = np.linspace(0, FRAME_SIZE * 2 - 1, FRAME_SIZE * 2)
     data = np.zeros(CHUNK * 2)
-    data[0: CHUNK] = dec_data
+    
     Ws_func_1st = 0
     # print(threshold)
     spectrum = np.zeros(len(angles))
-    Angles_dgree = angles*180/np.pi
+    Angles_dgree = np.round(angles*180/np.pi).astype(np.int8)
 
     plt.ion()
 
@@ -52,11 +48,16 @@ try:
     line, = ax.plot(Angles_dgree, spectrum)
 
     plt.ylim(0, 150)
-    plt.grid()
+    plt.grid(which='major', axis='x', linestyle='-', linewidth=0.5)
+    plt.grid(which='minor', axis='x', linestyle=':', linewidth=0.5)
+    plt.minorticks_on()
+    plt.gca().xaxis.set_minor_locator(plt.MultipleLocator(5))
     plt.xlabel('degree')
     plt.ylabel('spec')
 
-    
+    dec_data = np.frombuffer(recv_data(CHUNK* SAMP_WIDTH , server_socket), dtype= np.int16)
+    data[0: CHUNK] = dec_data
+
     while(True):
         count_frame = 1
         check = 0
@@ -84,31 +85,27 @@ try:
                 check = 1
                 # print("voice frame:", count_frame)
                 length = len(channel_1)
-                channel_1_fft = fft(channel_1 * np.hamming(length))
-                channel_2_fft = fft(channel_2 * np.hamming(length))
-                channel_3_fft = fft(channel_3 * np.hamming(length))
-                channel_4_fft = fft(channel_4 * np.hamming(length))
+                channel_1_fft = fft(channel_1)
+                channel_2_fft = fft(channel_2)
+                channel_3_fft = fft(channel_3)
+                channel_4_fft = fft(channel_4)
                 spec = []
                 # loai bo cac tan so khong can thiet
-                mic1_theta = pass_band(channel_1_fft, 150, 2500, samp_rate)
-                mic2_theta = pass_band(channel_2_fft, 150, 2500, samp_rate)
-                mic3_theta = pass_band(channel_3_fft, 150, 2500, samp_rate)
-                mic4_theta = pass_band(channel_4_fft, 150, 2500, samp_rate)
+                channel_1 = ifft(pass_band(channel_1_fft, 150, 2500, samp_rate))
+                channel_2 = ifft(pass_band(channel_2_fft, 150, 2500, samp_rate))
+                channel_3 = ifft(pass_band(channel_3_fft, 150, 2500, samp_rate))
+                channel_4 = ifft(pass_band(channel_4_fft, 150, 2500, samp_rate))
                 # su dung PSCM voi tung goc
                 for i in range(0, len(angles)):
-                    freq = array * samp_rate / length
-                        
-                    mic1_j = mic1_theta
-                    # mic1_data = ifft(mic1_j)
+                    
+                    mic1_data = channel_1
+                    mic2_data = arr_shift(channel_2, int(np.round(-1 * elements_distance * np.sin(angles[i])/sound_velo * samp_rate)))
+                    mic3_data = arr_shift(channel_3, int(np.round(-2 * elements_distance * np.sin(angles[i])/sound_velo * samp_rate)))
+                    mic4_data = arr_shift(channel_4, int(np.round(-3 * elements_distance * np.sin(angles[i])/sound_velo * samp_rate)))
 
-                    mic2_j = mic2_theta * np.exp(1j * 2 * np.pi * freq *( elements_distance * np.sin(angles[i])/sound_velo))
-                    # mic2_data = ifft(mic2_j)
+                    n = int(abs(np.round(-3 * elements_distance * np.sin(angles[i])/sound_velo * samp_rate)))
 
-                    mic3_j = mic3_theta * np.exp(1j * 2 * np.pi * freq * 2* elements_distance * np.sin(angles[i])/sound_velo)
-                    # mic3_data = ifft(mic3_j)
-                    mic4_j = mic4_theta * np.exp(1j * 2 * np.pi * freq * 3* elements_distance * np.sin(angles[i])/sound_velo)
-
-                    matrix_data = np.array([mic1_j, mic2_j, mic3_j, mic4_j])
+                    matrix_data = np.array([mic1_data[n: (length - n)], mic2_data[n: (length - n)], mic3_data[n: (length - n)], mic4_data[n: (length - n)]])
                     CovMat = matrix_data @ matrix_data.conj().transpose()
                     spec_i = music(CovMat, num_elements, angles[i], num_sources)
                     spec = np.append(spec, spec_i)
@@ -116,7 +113,7 @@ try:
             # ngat khoi vong lap va xoa hang doi khi thoi gian xu ly qua lau
             loop_time = time.time() - start_time
             
-            if(loop_time > 0.25 and count_frame < 10):
+            if(loop_time > 0.4 and count_frame < 10):
                 clear_data = recv_data((10 - count_frame)* CHUNK * SAMP_WIDTH, server_socket)
                 data[CHUNK:] = np.frombuffer(clear_data, dtype= np.int16)[-CHUNK:]
                 break
